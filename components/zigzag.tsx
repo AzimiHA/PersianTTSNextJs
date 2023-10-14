54cfffce59d1e2464df9bd9526df8b8117cfaf6d
Amir Azimi <amirazimi1999@gmail.com>
2023-10-14 02:05:34 -0400

    first commit

---
+++ components/zigzag.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+
+import FeatImage01 from '@/public/images/features-03-image-01.png'
+import FeatImage02 from '@/public/images/features-03-image-02.png'
+import FeatImage03 from '@/public/images/features-03-image-03.png'
+
+export default function Zigzag() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-6 md:py-10 border-t border-gray-800">
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+            <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">Reach goals that matter</div>
+            <h1 className="h2 mb-4">One product, unlimited solutions</h1>
+            <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum — semper quis lectus nulla.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
